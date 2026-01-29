fc655651cc98745cca5c35a0c192623897a95da5
minikube <minikube@gmail.com>
2026-01-29 00:59:04 +0000

    ss

--- add_user_script.sh
+++ add_user_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CLIENT=n.uzumaki
-IP=10.10.0.4
+CLIENT=test
+IP=10.10.0.5
 
 mkdir -p /root/wireguard/${CLIENT} 
 wg genkey | tee /root/wireguard/${CLIENT}/${CLIENT}_priv.key | wg pubkey > /root/wireguard/${CLIENT}/${CLIENT}_pub.key
@@ -10,15 +10,17 @@ cat > /root/wireguard/${CLIENT}/${CLIENT}.conf <<EOF
 [Interface]
 PrivateKey = $(cat /root/wireguard/${CLIENT}/${CLIENT}_priv.key)
 Address = ${IP}/24
-DNS = 1.1.1.1
+DNS = 10.10.0.1/24
 
 [Peer]
 PublicKey = $(cat /root/wireguard/server_public.key)
 Endpoint = 46.101.254.171:51820
-AllowedIPs = 0.0.0.0/0
+AllowedIPs = 10.10.0.0/24
 PersistentKeepalive = 25
 EOF
 
 wg set wg0 peer $(cat /root/wireguard/${CLIENT}/${CLIENT}_pub.key) allowed-ips ${IP}/32
 wg-quick save wg0
 wg show
+
+
